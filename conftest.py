a7b50fb775e7961c5241ec42c5c65696cf3a4d78
Farooq Abdullah <farooqspecials@gmail.com>
2026-08-01 18:32:20 +0300

    Implement cross-browser support for Chrome, Edge, and Firefox

--- conftest.py
+++ conftest.py
@@ -1,46 +1,64 @@
-import os
 import pytest
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        help="Browser to run tests: chrome, edge or firefox"
+    )
 
 
 @pytest.fixture()
-def driver():
+def driver(request):
 
-    options = Options()
+    browser = request.config.getoption("--browser").lower()
 
-    options.add_argument("--incognito")
+    if browser == "chrome":
+        options = Options()
+        options.add_argument("--incognito")
+        driver = webdriver.Chrome(
+            service=ChromeService(
+                ChromeDriverManager().install()
+            ),
+             options=options
+        )
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    elif browser == "edge":
+
+        driver = webdriver.Edge(
+            service=EdgeService(
+                EdgeChromiumDriverManager().install()
+            )
+        )
+
+    elif browser == "firefox":
+
+        driver = webdriver.Firefox(
+            service=FirefoxService(
+                GeckoDriverManager().install()
+            )
+        )
+
+    else:
+        raise ValueError(
+            f"Unsupported browser: {browser}"
+        )
 
     driver.maximize_window()
 
     yield driver
 
     driver.quit()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-
-    outcome = yield
-    report = outcome.get_result()
-
-    if report.when == "call" and report.failed:
-
-        driver = item.funcargs.get("driver")
-
-        if driver:
-
-            os.makedirs("screenshots", exist_ok=True)
-
-            screenshot_path = f"screenshots/{item.name}.png"
-
-            driver.save_screenshot(screenshot_path)
-
-            print(f"\nScreenshot saved: {screenshot_path}")
